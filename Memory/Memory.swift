a46d94c3a87dd3b87026e0036435ea634d0796de
Nada Yehia Dawoud <nadawoud@gmail.com>
2018-06-05 22:21:17 +0200

    Initial commit

---
+++ Memory/Memory.swift
@@ -0,0 +1,81 @@
+//
+//  Memory.swift
+//  Memory
+//
+//  Created by Nada Dawoud on 5/30/18.
+//  Copyright © 2018 Mobile Apps Kitchen. All rights reserved.
+//
+
+import Foundation
+class Memory {
+    
+    var cards = [Card]()
+    
+    private(set) var score = 0
+    
+    private(set) var flipCount = 0
+    
+    private var indexOfOnlyFaceUpCard: Int?
+    
+    init (numberOfPairsOfCards: Int) {
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        cards = shuffleCards()
+    }
+    
+    func chooseCard(at index: Int) {
+        flipCount += 1
+        
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOnlyFaceUpCard, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                    score += 2
+                } else {
+                    if cards[index].seen > 1 {
+                        score -= 1
+                    }
+                    if cards[indexOfOnlyFaceUpCard!].seen > 1 {
+                        score -= 1
+                    }
+                }
+                cards[index].seen += 1
+                cards[indexOfOnlyFaceUpCard!].seen += 1
+                cards[index].isFaceUp = true
+                indexOfOnlyFaceUpCard = nil
+            }
+            else {
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOnlyFaceUpCard = index
+                cards[indexOfOnlyFaceUpCard!].seen += 1
+            }
+        }
+    }
+    
+    func shuffleCards() -> [Card] {
+        var shuffledCards = [Card]()
+        for _ in cards.indices {
+            let randomIndex = Int(arc4random_uniform(UInt32(cards.count)))
+            shuffledCards.append(cards[randomIndex])
+            cards.remove(at: randomIndex)
+        }
+        return shuffledCards
+    }
+    
+    func reset() {
+        flipCount = 0
+        score = 0
+        cards = shuffleCards()
+        for index in cards.indices {
+            cards[index].isFaceUp = false
+            cards[index].isMatched = false
+            indexOfOnlyFaceUpCard = nil
+        }
+    }
+}
---
+++ Memory/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  Memory
+//
+//  Created by Nada Dawoud on 5/30/18.
+//  Copyright © 2018 Mobile Apps Kitchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    lazy var game = Memory(numberOfPairsOfCards: (cardButtons.count + 1) / 2 )
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+   
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardNumber = cardButtons.index(of: sender) {
+            game.chooseCard(at: cardNumber)
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        flipCountLabel.text = "Flips: \(game.flipCount)"
+        scoreLabel.text = "Score: \(game.score)"
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card ), for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 0.8470588235, green: 0.8588235294, blue: 0.8862745098, alpha: 1)
+            }
+            else {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 0) : #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+            }
+        }
+    }
+    
+    var themeEmoji = ["Faces": ["🤠", "🤣", "😇", "😍", "😱", "🤪", "🤩", "🤓", "🧐", "😴"],
+                  "Animal Faces": ["🐶", "🦁", "🐵", "🐰", "🐼", "🐯","🐴","🐺", "🐮", "🐨"],
+                  "Sea Animals": ["🐙", "🦐", "🦀", "🐠", "🐳", "🐬", "🐋", "🦈", "🐡", "🦑"],
+                  "Sports": ["⚽️", "🏀", "🏈", "⚾️", "🎾", "🏐", "🏉", "🎱", "🏓", "🥊"],
+                  "Random" : ["🔮", "💎", "🎉", "🎊", "👾", "🤖", "✨", "🎲", "🎮", "🎯"],
+                  "Flags": ["🇦🇹", "🇦🇺", "🇩🇿", "🇨🇦", "🇪🇬", "🇬🇹", "🇰🇷", "🇺🇸", "🇬🇧", "🇸🇩"],
+                  "Animals": ["🦓", "🐘", "🐪", "🦒", "🐄", "🐈", "🐿", "🐇", "🦌", "🐏"]]
+    
+    var emojiChoices = ["🤠", "🤣", "😇", "😍", "😱", "🤪", "🤩", "🤓", "🧐", "😴"]
+    
+    var emoji = [Int: String]()
+    
+    func randomizeTheme() {
+        emoji = [Int: String]()
+        
+        let themes = Array(themeEmoji.keys)
+        let randomIndex = Int(arc4random_uniform(UInt32(themes.count)))
+        if let emojies = themeEmoji[themes[randomIndex]] {
+            emojiChoices = emojies
+        }
+    }
+    
+    func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        return emoji[card.identifier] ?? "♦️"
+    }
+   
+    @IBAction func startNewGame(_ sender: UIButton) {
+        game.reset()
+        randomizeTheme()
+        updateView()
+    }
+}
+
